35623d7ba04fcc975b459e8256ce4604f48237cb
Achraf <achraf.triki@insat.ucar.tn>
2024-11-20 18:06:48 +0100

    team project

--- adminfront/README.md
+++ adminfront/README.md
@@ -1,6 +1,6 @@
 # events-admin
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.0.
+This project DEV-HUB was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.0.0.
 
 ## Development server
 
--- adminfront/src/app/services/guards/authenticate.guard.ts
+++ adminfront/src/app/services/guards/authenticate.guard.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree, } from '@angular/router';
 import { Observable } from 'rxjs';
 import { AuthenticationService } from '../authentication.service';
-
+// guard
 @Injectable()
 export class AuthenticateGuard implements CanActivate {
   constructor(private authenticationService: AuthenticationService, private router: Router) {}
